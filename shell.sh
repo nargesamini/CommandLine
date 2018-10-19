41859fe2eeaa225ca8232f0b68d55fc26f106b70
nargesamini <nargesaminiafg@gmail.com>
2018-10-19 20:59:24 +0300

    I added my homework

---
+++ shell.sh
@@ -0,0 +1,20 @@
+mkdir narges
+cd narges
+touch blank
+echo "Hello Hello Hello Hello Hello" > greeting.txt
+
+cp -r greeting.txt 1.txt
+cp -r greeting.txt 2.txt 
+cp -r greeting.txt 3.txt 
+cp -r greeting.txt 4.txt 
+cp -r greeting.txt 5.txt 
+ 
+echo "cat" > pets.txt
+echo "-dog" >> pets.txt
+echo "-hamster" >> pets.txt
+
+echo "cat" >command.txt
+echo " is" >>command.txt
+echo " pwd" >>command.txt
+
+diff pets.txt comands.txt>> lovelycommands.txt
